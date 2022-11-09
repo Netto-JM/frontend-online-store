6cabeb3fddc75233d7cbfaaa80c4ce4e822474c3
IzabelaFattori <izabela.fattori@hotmail.com>
2022-11-09 17:11:52 -0300

    requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
   state = {
     productsList: [],
+    categories: [],
+  };
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  fetchCategories = async () => {
+    const response = await getCategories();
+    this.setState({ categories: response });
   };
 
   render() {
-    const { productsList } = this.state;
+    const { productsList, categories } = this.state;
     return (
       <div>
         <label htmlFor="Home">
@@ -16,6 +27,18 @@ class Home extends Component {
           <p data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>)}
+
+        {categories.map((category) => (
+          <button
+            type="button"
+            key={ category.id }
+            data-testid="category"
+            onClick={ () => this.getProductsFromCategoryAndQuery(category.id) }
+          >
+            {category.name}
+          </button>
+        ))}
+
       </div>
     );
   }
