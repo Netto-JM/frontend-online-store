0cb15aa4055ef2ecd173bdf20617ac4a13bd8fff
giovane1230 <giovanef.96@gmail.com>
2022-11-10 15:48:03 -0300

    req6 implementando url

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -69,6 +69,7 @@ class Home extends Component {
               type="button"
             >
               Carrinho de Compras
+              {/* oi */}
             </button>
           </Link>
         </div>
