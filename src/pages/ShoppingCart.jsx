1816b125db918710483bffbff58860f55bc5b325
NettoJM <nettojm86@gmail.com>
2022-11-13 15:56:40 -0300

    quantidade retirada do shoppingCart

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { getCartItems, getTotalQuantity } from '../services/api';
+import { getCartItems } from '../services/api';
 import ItemCard from '../components/ItemCard';
 
 class ShoppingCart extends React.Component {
   state = {
     productsList: [],
-    totalQuantity: 0, // estado que guarda a quantidade de itens
   };
 
   componentDidMount() {
@@ -16,16 +15,13 @@ class ShoppingCart extends React.Component {
 
   toMountAndUpdate = () => {
     const productsList = getCartItems();
-    // const totalQuantity = productsList.length; para totais de itens únicos
-    const totalQuantity = getTotalQuantity(); // para total de itens mesmo repetidos
     this.setState({
       productsList,
-      totalQuantity,
     });
   };
 
   render() {
-    const { productsList, totalQuantity } = this.state;
+    const { productsList } = this.state;
 
     const itemList = productsList.map(({ id, item, quantity }) => (
       <ItemCard
@@ -51,8 +47,6 @@ class ShoppingCart extends React.Component {
           <button type="button" data-testid="checkout-products">
             Finalizar Compra
           </button>
-          <span data-testid="shopping-cart-size">{totalQuantity}</span>
-          {/* E aqui tentei exibir o total, está funcionando, mas o teste que analisa essa página não passa */}
         </Link>
       </div>
     );
